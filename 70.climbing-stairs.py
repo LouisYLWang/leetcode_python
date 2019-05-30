35c479f3821cf9c2920d27406938770013584cd6
louis.yl.wang <louis.yl.wang@outlook.com>
2019-05-30 15:46:10 +0800

    update 70 climbing-stairs

--- 70.climbing-stairs.py
+++ 70.climbing-stairs.py
@@ -5,5 +5,18 @@
 #
 class Solution:
     def climbStairs(self, n: int) -> int:
-        
+        sol_map = dict()
+        def get_res(n: int) -> int:
+            nonlocal sol_map
+            if n == 1:
+                sol_map[1] = 1
+                return 1
+            if n == 2:
+                sol_map[1] = 1
+                return 2
+            if n not in sol_map:
+                sol_map[n] =  get_res(n - 1) + get_res(n - 2)
+            return sol_map[n]
+
+        return get_res(n)
 
