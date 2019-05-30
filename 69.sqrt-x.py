bcce0e6019e5597b4667929851fc56da2e1a9389
louis.yl.wang <louis.yl.wang@outlook.com>
2019-05-30 12:06:04 +0800

    update 69 sqrt-x newton method

--- 69.sqrt-x.py
+++ 69.sqrt-x.py
@@ -4,21 +4,36 @@
 # [69] Sqrt(x)
 #
 class Solution:
+    # newton method:
+    def mySqrt(self, x: int) -> int:
+
+        if x == 1:
+            return x 
+
+        cur_a = x // 2
+        while cur_a * cur_a > x:
+            cur_a=(cur_a+(x//cur_a)) //2
+        else:
+            return cur_a
+
+    '''
+    dummy solution
     def mySqrt(self, x: int) -> int:
         cur_b = x // 2
         if x == 1:
             return x 
 
-        while cur_b**2 > x:
+        while cur_b * cur_b > x:
             cur_b = cur_b //2
         else:
-            if cur_b**2 == x:
+            if cur_b * cur_b == x:
                 return cur_b
-            if cur_b**2 < x:
-                while not cur_b**2 > x:
+            if cur_b * cur_b < x:
+                while not cur_b * cur_b > x:
                     cur_b += 1
                 else:
                     cur_b -= 1
                     return cur_b
+    '''
 
         
