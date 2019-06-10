4e30527145783c9e581b9c6e48d67ef4dd18b752
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-10 10:39:02 +0800

    update 104 maximum-depth-of-binary-tree

--- 104.maximum-depth-of-binary-tree.py
+++ 104.maximum-depth-of-binary-tree.py
@@ -12,25 +12,14 @@
 
 class Solution:
     def maxDepth(self, root: TreeNode) -> int:
-        #count = 0
-        #max_dep = 1
-        def DFS(cur):
-            #nonlocal count
-            #nonlocal max_dep
-
-            if cur is not None:
-                if (cur.left is None) & (cur.right is None):
-                    return 1
-                if (cur.left is not None) | (cur.right is not None):
-                    return max(DFS(cur.left), DFS(cur.right)) + 1
+        cur = root
+        if cur is not None:
+            if (cur.left is None) & (cur.right is None):
+                return 1
+            if (cur.left is not None) | (cur.right is not None):
+                return max(self.maxDepth(cur.left), self.maxDepth(cur.right)) + 1
+        else:
             return 0
-
-        if root is None:
-            return 0
-        
-
-        return DFS(root)
-        #return #count + 1
 
 
 
