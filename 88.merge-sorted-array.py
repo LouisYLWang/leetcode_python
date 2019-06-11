92d4e0c1c89a63a09b3e24aad15e63afc9983859
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-11 20:27:09 +0800

    update 88 merge-sorted-array

--- 88.merge-sorted-array.py
+++ 88.merge-sorted-array.py
@@ -4,9 +4,17 @@
 # [88] Merge Sorted Array
 #
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        
+    def merge(self, nums1, m, nums2, n):
+
+        if not m:
+            nums1[:] = nums2[:]
+        else:
+            while m > 0 and n > 0:
+                if nums1[m-1] >= nums2[n-1]:
+                    nums1[m+n-1], nums1[m-1] = nums1[m-1], nums1[m+n-1]
+                    m -= 1
+                else:
+                    nums1[m+n-1], nums2[n-1] = nums2[n-1], nums1[m+n-1]
+                    n -= 1
+        nums1[:n] = nums2[:n]
 
