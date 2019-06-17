ab3f8e0dbad6b7ecfc8711736af5a73132250a2e
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-17 10:49:53 +0800

    update 112 path-sum

--- 112.path-sum.py
+++ 112.path-sum.py
@@ -29,4 +29,21 @@ class Solution:
 
         getPathSum(root)
         return sum in path_sum
+'''
+class Solution:
+    # @param root, a tree node
+    # @param sum, an integer
+    # @return a boolean
+    # 1:27
+    def hasPathSum(self, root, sum):
+        if not root:
+            return False
+
+        if not root.left and not root.right and root.val == sum:
+            return True
+        
+        sum -= root.val
+
+        return self.hasPathSum(root.left, sum) or self.hasPathSum(root.right, sum)
+'''
 
