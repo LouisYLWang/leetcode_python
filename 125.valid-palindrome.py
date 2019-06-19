3c17d0825e32377b137176b5fc18e575333be268
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-19 15:28:55 +0800

    update 125 valid palindrome (dummy method)

---
+++ 125.valid-palindrome.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=125 lang=python3
+#
+# [125] Valid Palindrome
+#
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphabet =  'abcdefghijklmnopqrstuvwxyz0123456789'
+        s = s.lower()
+        
+        while len(s)>1 :
+            while s and s[0] not in alphabet:
+                s = s[1:]
+
+            while s and s[-1] not in alphabet:
+                s = s[:-1]
+            if s:
+                if s[0] == s[-1]:
+                    s = s[1:-1]
+                else:return False
+
+        return True
+''''        
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        alphabet =  'abcdefghijklmnopqrstuvwxyz0123456789'
+        s = s.lower() 
+        s_rev = ""     
+        while s != s_rev :
+            while s and s[0] not in alphabet:
+                s = s[1:]
+            while s and s[-1] not in alphabet:
+                s = s[:-1]
+            if s:
+                s_rev = s[0] + s_rev
+                s = s[1:]
+            
+            #if s:
+            #    if s[0] == s[-1]:
+            #        s = s[1:-1]
+            #    else:return False
+
+        return s == s_rev
+
+'''
+
+
