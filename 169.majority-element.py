fd6ddd075583557b7801b7d82ea0353a2886d428
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-23 01:08:24 +0800

    update 169 majority element (boyer - moore algorithm )

--- 169.majority-element.py
+++ 169.majority-element.py
@@ -4,6 +4,21 @@
 # [169] Majority Element
 #
 class Solution(object):
+    # boyer - moore algorithm 
+    def majorityElement(self, nums):
+        flag = 0
+        cur_mode = None
+        for i in nums:
+            if flag == 0:
+                cur_mode = i
+            if i == cur_mode:
+                flag += 1
+            else:
+                flag -= 1
+        return cur_mode
+
+    # self implemented
+    '''
     def majorityElement(self, nums):
         """
         :type nums: List[int]
@@ -11,5 +26,6 @@ class Solution(object):
         """
         nums.sort()
         return nums[len(nums)//2]
-        
+    '''
+    
 
