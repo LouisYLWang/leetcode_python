23d5a385431f392c7741438e8a25a2dbb81b312c
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-26 16:45:57 +0800

    update 198 house-robber (iteration method)

--- 198.house-robber.py
+++ 198.house-robber.py
@@ -4,6 +4,7 @@
 # [198] House Robber
 #
 #recursion method
+
 class Solution(object):
     def rob(self, nums):
         """
@@ -23,5 +24,22 @@ class Solution(object):
             return subproblem(len(nums))
         return 0
 
-        
+#iteration method
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        if n == 0:
+            return 0
+        elif n == 1:
+            return nums[0]
+        cur_l = nums[0]  # n = 1
+        cur_r = max(nums[0], nums[1]) # n = 2 
 
+        for i in nums[2:]:
+            cur_max = max(cur_l + i, cur_r)
+            cur_l, cur_r =  cur_r, cur_max
+        return cur_r
