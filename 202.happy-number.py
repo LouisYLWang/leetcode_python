ac8ac903121184c992caca53a27acc07b4c0da83
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-27 23:48:44 +0800

    update 202 happy number

--- 202.happy-number.py
+++ 202.happy-number.py
@@ -4,10 +4,29 @@
 # [202] Happy Number
 #
 class Solution(object):
+    table_a = {0:0}
+    table_b = set()
     def isHappy(self, n):
         """
         :type n: int
         :rtype: bool
         """
+        if (n in self.table_b) or n == 2 or n == 3:
+            return False
+        
+        res = 0
+        for i in str(n):
+            cur = int(i)
+            if cur in self.table_a:
+                res += self.table_a[cur]
+            else:
+                self.table_a[cur] = cur**2
+                res += cur**2
+        if res == 1:
+            return True
+        print(res)
+        self.table_b.add(n)
+        return self.isHappy(res)
+
         
 
