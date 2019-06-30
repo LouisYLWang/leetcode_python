80463c5f445117f4e18dc553cc36b2c541001462
louis.yl.wang <louis.yl.wang@outlook.com>
2019-06-30 23:46:04 +0800

    update 189 rotate array (O(1) space)

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -4,6 +4,32 @@
 # [189] Rotate Array
 #
 class Solution(object):
+    #O(1) space
+    def rotate(self, nums, k):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
+    n = len(nums)
+    if k > n:
+        k = k - n
+
+    def reverse(start, end):
+        nonlocal nums
+        while start < end:
+            temp = nums[start]
+            nums[start] = nums[end]
+            nums[end] = temp
+            start += 1
+            end -= 1 
+    
+    reverse(0, n-1)
+    reverse(0, k-1)
+    reverse(k, n-1)
+
+    #O(n) space
+    
     def rotate(self, nums, k):
         """
         :type nums: List[int]
@@ -14,5 +40,6 @@ class Solution(object):
         if k > n:
             k = k - n
         nums[:] = nums[-k:] + nums[:-k]
+    
 
             
