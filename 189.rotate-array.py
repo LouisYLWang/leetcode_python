ea4130e269a559a54dfbf75e3fa69b2e7958907d
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-01 17:12:37 +0800

    update 189 rotate array

--- 189.rotate-array.py
+++ 189.rotate-array.py
@@ -5,18 +5,16 @@
 #
 
 class Solution(object):
+    """
+    :type nums: List[int]
+    :type k: int
+    :rtype: None Do not return anything, modify nums in-place instead.
+    """
     def rotate(self, nums, k):
-        """
-        :type nums: List[int]
-        :type k: int
-        :rtype: None Do not return anything, modify nums in-place instead.
-        """
         n = len(nums)
         if k > n:
             k = k - n
-
-        def reverse(start, end):
-            nonlocal nums
+        def reverse(start, end, nums):
             while start < end:
                 temp = nums[start]
                 nums[start] = nums[end]
@@ -24,10 +22,11 @@ class Solution(object):
                 start += 1
                 end -= 1 
         
-        reverse(0, n-1)
-        reverse(0, k-1)
-        reverse(k, n-1)
-        
+        reverse(0, n-1, nums)
+        reverse(0, k-1, nums)
+        reverse(k, n-1, nums)
+        pass
+    
 
     #O(n) space
     '''
---
+++ 217.contains-duplicate.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=217 lang=python
+#
+# [217] Contains Duplicate
+#
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        
+
