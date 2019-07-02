05906c428a70829c20e21e7f47d47de0808c2bcf
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-03 00:54:13 +0800

    update 231 power of two

---
+++ 231.power-of-two.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=231 lang=python
+#
+# [231] Power of Two
+#
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n == 0:
+            return False
+        if n == 1:
+            return True
+        return int(bin(n)[3:]) == 0 and int(bin(n)[2]) == 1
+
+
