bc2fbc44659f51628a2566f9795076de4901d7e7
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-04 22:31:39 +0800

    update 234 palindrome linked list'

--- 234.palindrome-linked-list.py
+++ 234.palindrome-linked-list.py
@@ -38,7 +38,43 @@ class Solution(object):
                     head = head.next
                     cur = cur.next
         return True
+
+#
+'''
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if head:
+            fast, slow = head, head
+            while fast and fast.next:
+                fast = fast.next.next
+                slow = slow.next
         
+            def reverse(head):
+                prev = None
+                cur = head
+                while cur.next:
+                    temp = cur.next
+                    cur.next = prev
+                    prev = cur 
+                    cur = temp
+                cur.next = prev
+                return cur
+
+            head2 = reverse(slow)
+            cur = head2
+
+            while cur:
+                if head.val != cur.val:
+                    return False 
+                else:
+                    head = head.next
+                    cur = cur.next
+        return True
+        
+'''
         
 
 
