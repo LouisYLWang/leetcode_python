e6af50b2356cc0c3101a9051b7692783e7143074
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-09 01:14:20 +0800

    update 278 First Bad Version

--- 278.first-bad-version.py
+++ 278.first-bad-version.py
@@ -7,6 +7,10 @@
 # @param version, an integer
 # @return a bool
 # def isBadVersion(version):
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
 
 class Solution(object):
     def firstBadVersion(self, n):
@@ -14,15 +18,22 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        cur_i = n//2
-        while not isBadVersion(cur_i):
-            cur_i //= 2
-        print(cur_i)
-        while isBadVersion(cur_i):
-            cur_i += 1
-        print(cur_i)
-        return cur_i
-
+        if n:
+            upper = n
+            lower = 0
+            upper_pre = upper
+            lower_pre = lower
+            while upper - lower != 1:
+                upper_pre = upper
+                upper = (upper + lower) //2
+                if not isBadVersion(upper):
+                    upper = upper_pre
+                    if not isBadVersion(lower):
+                        lower_pre = lower
+                        lower = (upper + lower) //2
+                    else:
+                        lower = lower_pre
+            return upper
 
         
 
