dbc32acab4f9b255191162accd4a420dcdc0c3e8
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-08 21:54:32 +0800

    263 Ugly Number

--- 263.ugly-number.py
+++ 263.ugly-number.py
@@ -4,10 +4,16 @@
 # [263] Ugly Number
 #
 class Solution(object):
-    def isUgly(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-        
+    def isUgly(self, num: int):
+        if num:
+            while num%2 == 0:
+                num /=2
+            while num%3 == 0:
+                num /=3
+            while num%5 == 0:
+                num /=5
+            if num == 1:
+                return True
+        return False
+
 
