3f4f5cc6740221e8cbae3122e57f854b302ae1e5
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-09 23:36:32 +0800

    update 283 Move Zeroes

--- 283.move-zeroes.py
+++ 283.move-zeroes.py
@@ -3,7 +3,23 @@
 #
 # [283] Move Zeroes
 #
+
+
 class Solution(object):
+    def moveZeroes(self, nums):
+        zero = 0
+        i = 0
+        while i < len(nums):
+            if nums[i] != 0:
+                nums[i], nums[zero] = nums[zero], nums[i]
+                zero += 1
+            i += 1
+    
+    # for this question, my version rebuit the value of nonzero index every time.
+    # and I probably use way too much if sentence, which would slow down the process
+
+    #SELF IMPLEMENTED VERSION
+    '''
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
@@ -12,7 +28,7 @@ class Solution(object):
         zero = 0
         nonzero = 0
         numsize = len(nums)
-
+        
         while zero < numsize and nonzero < numsize - 1:
             if nums[zero] == 0:
                 nonzero = zero
@@ -21,3 +37,4 @@ class Solution(object):
                 nums[zero], nums[nonzero] = nums[nonzero], nums[zero]                
             else:
                 zero += 1
+    '''
