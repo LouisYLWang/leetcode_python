9e0be4c99287394afd428d389ed436022ed3e564
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-11 20:17:30 +0800

    update 344 Reverse String

---
+++ 344.reverse-string.py
@@ -0,0 +1,19 @@
+#
+# @lc app=leetcode id=344 lang=python
+#
+# [344] Reverse String
+#
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            s[i], s[j] = s[j], s[i]
+            i += 1
+            j -= 1 
+        
+
