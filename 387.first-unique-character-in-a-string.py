bc7cbb03e5d0589031931bd102e2b4175ad08621
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-13 23:54:49 +0800

    update 387 First Unique Character in a String

--- 387.first-unique-character-in-a-string.py
+++ 387.first-unique-character-in-a-string.py
@@ -4,6 +4,7 @@
 # [387] First Unique Character in a String
 #
 class Solution(object):
+    '''
     def firstUniqChar(self, s):
         """
         :type s: str
@@ -16,6 +17,24 @@ class Solution(object):
         i = 0
         while i < s_len:
             if bitmap[ord(s[i])-ord('a')] == 1:
+                return i 
+            i += 1
+        return -1
+    '''
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        map = dict()
+        for i in s:
+            if i not in map:
+                map[i] = 0
+            map[i] += 1
+        s_len = len(s)
+        i = 0
+        while i < s_len:
+            if map[s[i]] == 1:
                 return i 
             i += 1
         return -1
