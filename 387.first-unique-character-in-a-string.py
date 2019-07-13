aa73345f05416dfdea1fef7b6eced3bc7afadcda
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-13 23:48:23 +0800

    update 387 First Unique Character in a String

---
+++ 387.first-unique-character-in-a-string.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=387 lang=python
+#
+# [387] First Unique Character in a String
+#
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        bitmap = [0]*26
+        for i in s:
+            bitmap[ord(i)-ord('a')] += 1
+        s_len = len(s)
+        i = 0
+        while i < s_len:
+            if bitmap[ord(s[i])-ord('a')] == 1:
+                return i 
+            i += 1
+        return -1
