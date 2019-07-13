a85f2b80fc5d01f4eecd523d834d738fa68ecd6a
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-13 23:59:20 +0800

    update 389 Find the Difference

---
+++ 389.find-the-difference.py
@@ -0,0 +1,18 @@
+#
+# @lc app=leetcode id=389 lang=python
+#
+# [389] Find the Difference
+#
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        target = s+t
+        res = 0
+        for i in target:
+            res ^= ord(i)
+        return i
+
