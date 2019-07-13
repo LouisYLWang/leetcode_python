728c750bf468b1ef9147a5f17d39a1631a6ef5db
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-13 22:30:12 +0800

    update 383 Ransom Note

--- 383.ransom-note.py
+++ 383.ransom-note.py
@@ -3,7 +3,23 @@
 #
 # [383] Ransom Note
 #
+
 class Solution(object):
     def canConstruct(self, ransomNote, magazine):
+        """
+        :type ransomNote: str
+        :type magazine: str
+        :rtype: bool
+        """
+        bitmap = [0]*26
+        for i in ransomNote:
+            bitmap[ord(i)-ord('a')] +=1
+        
+        for i in magazine:
+            if bitmap[ord(i)-ord('a')] > 0:
+                bitmap[ord(i)-ord('a')] -= 1
+        return sum(bitmap) == 0
+    '''
+    def canConstruct(self, ransomNote, magazine):
         return all(ransomNote.count(c)<=magazine.count(c) for c in ransomNote)
-
+    '''
