6128f5f4648a38ed1ef072ea3381d328143ad2e3
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-15 17:51:21 +0800

    update 401 Binary Watch

--- 401.binary-watch.py
+++ 401.binary-watch.py
@@ -4,20 +4,44 @@
 # [401] Binary Watch
 #
 class Solution(object):
-    def findNthDigit(self, n):
+    def readBinaryWatch(self, num):
         """
-        :type n: int
-        :rtype: int
+        :type num: int
+        :rtype: List[str]
         """
-        i = 1
-        cur = 10
-        while n > cur:
-            i += 1
-            cur += 9 * i * 10 ** (i - 1)
-        
-        cur -= 9 * i * 10 ** (i - 1)
-        num = (n - cur)//i 
-        digit = (n - cur)% i 
+        def get_bin(n, bits):
 
-        return int(str(10**(i-1) + num)[digit])
+            if n == 0:
+                return set([0])
+            if n == 1:
+                return set([1<<i for i in range(0,bits)] )
+            if n == bits:
+                return set([sum(get_bin(1, bits))])
+            res = set()
+            for i in range(1, n):
+                for j in get_bin(i, bits):
+                    for k in get_bin(n-i, bits):
+                        if j|k != j and j|k != k:
+                            res.add(j|k)
+                        
+                            
+            return res - get_bin(n-1, bits)
+        
+
+        res = []
+        hour = 0
+        while hour < 5:
+            minutes = num - hour
+            for i in get_bin(hour ,4):
+                for j in get_bin(minutes, 6):
+                    if i < 12:
+                        if 10 <= j < 60:
+                            res.append("%i:%i" %(i,j))
+                        if j < 10:
+                            res.append("%i:0%i" %(i,j))
+            hour += 1
+        res.sort()
+        return res
+ 
+
 
