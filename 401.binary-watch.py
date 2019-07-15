65460a5b58f611a716629b5b8a76fe6e5cc8ec08
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-15 23:16:46 +0800

    update 401 Binary Watch

--- 401.binary-watch.py
+++ 401.binary-watch.py
@@ -9,39 +9,30 @@ class Solution(object):
         :type num: int
         :rtype: List[str]
         """
+
         def get_bin(n, bits):
-
             if n == 0:
-                return set([0])
-            if n == 1:
-                return set([1<<i for i in range(0,bits)] )
-            if n == bits:
-                return set([sum(get_bin(1, bits))])
-            res = set()
-            for i in range(1, n):
-                for j in get_bin(i, bits):
-                    for k in get_bin(n-i, bits):
-                        if j|k != j and j|k != k:
-                            res.add(j|k)
-                        
-                            
-            return res - get_bin(n-1, bits)
+                return [0]
+            res = list()
+            digit = n-1
+            cur = 1 << digit
+            while cur < 1 << bits:
+                for i in get_bin(n-1, digit):
+                    res.append(cur|i)
+                digit += 1
+                cur = 1 << digit
+            return res
         
-
         res = []
         hour = 0
-        while hour < 5:
-            minutes = num - hour
+        while hour < 5 and hour <= num:
+            minutes = num - hour            
             for i in get_bin(hour ,4):
                 for j in get_bin(minutes, 6):
-                    if i < 12:
-                        if 10 <= j < 60:
-                            res.append("%i:%i" %(i,j))
-                        if j < 10:
-                            res.append("%i:0%i" %(i,j))
+                    if i < 12 and j < 60:
+                        res.append("%d:%.2d" %(i,j))
             hour += 1
-        res.sort()
         return res
- 
+        
 
 
