46c1aa9117898561eec8b84934c9ece1d5a14ec1
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-15 14:01:39 +0800

    update 400 Nth Digit

--- 400.nth-digit.py
+++ 400.nth-digit.py
@@ -9,5 +9,14 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        i = 1
+        cur = 10
+        while n > cur:
+            i += 1
+            cur += 9 * i * 10 ** (i - 1)
         
+        cur -= 9 * i * 10 ** (i - 1)
+        num = (n - cur)//i 
+        digit = (n - cur)% i 
 
+        return int(str(10**(i-1) + num)[digit])
