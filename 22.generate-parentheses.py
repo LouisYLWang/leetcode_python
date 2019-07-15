1d439456faecff0593300531b5f7447b6685b0b2
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-15 10:17:10 +0800

    update 22 Generate Parentheses

--- 22.generate-parentheses.py
+++ 22.generate-parentheses.py
@@ -7,21 +7,13 @@ class Solution:
     def generateParenthesis(self, n):
         if n == 0:
             return [""]
-        if n == 1:
-            return ["()"]
-        if n == 2:
-            return ["(())","()()"]
-    
-        res = ["(" + self.generateParenthesis(n-1)[0]+")", "(" + self.generateParenthesis(n-1)[-1]+")"]
-        
-        for i in self.generateParenthesis(n-1)[1:-1]:
-            res.append("(" + i + ")")
-            res.append(i + "()")
-            res.append("()" + i)
-            
-        for i in range(1,n):
-            res.append(self.generateParenthesis(i)[0] + self.generateParenthesis(n-i)[0])
-        res.append(self.generateParenthesis(n-1)[-1] +"()")
+        res = list()
+
+        for i in range(0,n):
+            for j in self.generateParenthesis(i):
+                for k in self.generateParenthesis(n-i-1):
+                    res.append("(" + j + ")" + k)
         return res
 
-        #"()(())()"
+
+
