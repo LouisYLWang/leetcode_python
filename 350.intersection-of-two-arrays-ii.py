79ab675f52f846e99ccd190d07e7abc153065360
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-16 16:01:47 +0800

    update 350 Intersection of Two Arrays II

--- 350.intersection-of-two-arrays-ii.py
+++ 350.intersection-of-two-arrays-ii.py
@@ -10,5 +10,23 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        
+        #sorted version:
+        nums1.sort()
+        nums2.sort()
+        len_1 = len(nums1)
+        len_2 = len(nums2)
+        i, j = 0,0 
+        res = []
+        while i < len_1 and j < len_2:
+            if nums1[i] < nums2[j]:
+                i += 1
+            elif nums2[j] < nums1[i]:
+                j += 1
+            elif nums2[j] == nums1[i]:
+                res.append(nums1[i])
+                i += 1
+                j += 1
+        return res
+
+
 
