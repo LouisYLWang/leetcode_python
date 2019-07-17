d1e6e602b165b955b3a6ecf0d2204eff3cd99f3d
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-17 22:51:13 +0800

    414 Third Maximum Number

--- 414.third-maximum-number.py
+++ 414.third-maximum-number.py
@@ -9,5 +9,26 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        #theoratical works but with not on special cases
+        max_ = max(nums)
+        min_ = min(nums)      
+        bitmap = [0] * (max_ - min_ + 1)
+        count = 0
+        for i in nums:
+            if bitmap[max_ - i] != 1:
+                bitmap[max_ -i] += 1
+                count += 1
         
+        if count < 3:
+            return max_
+        
+        else:
+            i = 0
+            find = 0
+            while find < 3:
+                if bitmap[i] == 1:
+                    find += 1
+                i += 1
+            return max_ - i + 1
+
 
---
+++ 427.construct-quad-tree.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=427 lang=python
+#
+# [427] Construct Quad Tree
+#
+"""
+# Definition for a QuadTree node.
+class Node(object):
+    def __init__(self, val, isLeaf, topLeft, topRight, bottomLeft, bottomRight):
+        self.val = val
+        self.isLeaf = isLeaf
+        self.topLeft = topLeft
+        self.topRight = topRight
+        self.bottomLeft = bottomLeft
+        self.bottomRight = bottomRight
+"""
+class Solution(object):
+    def construct(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: Node
+        """
+        
+
