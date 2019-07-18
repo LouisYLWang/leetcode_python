318a53ca5a6103bde406bc89cfa3cd2a6d7909af
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-18 22:59:58 +0800

    update 434  Number of Segments in a String

---
+++ 434.number-of-segments-in-a-string.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=434 lang=python
+#
+# [434] Number of Segments in a String
+#
+class Solution(object):
+    def countSegments(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        a = s.split()
+        return len(a) 
+
