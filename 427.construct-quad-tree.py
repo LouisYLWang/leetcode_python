af15f1d873e9cb50703bba14c4ee7f2bf84279c5
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-20 11:03:00 +0800

    update 427 Construct Quad Tree

--- 427.construct-quad-tree.py
+++ 427.construct-quad-tree.py
@@ -14,11 +14,35 @@ class Node(object):
         self.bottomLeft = bottomLeft
         self.bottomRight = bottomRight
 """
+
 class Solution(object):
     def construct(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: Node
         """
+        gl = len(grid)
+        isLeaf = True
+        topLeft, topRight, bottomLeft, bottomRight = None, None, None, None
+        if sum(map(sum, grid)) == sum(map(len, grid)):
+            val = True
+        elif sum(map(sum, grid)) == 0:
+            val = False
+        else:
+            val = "*"
+            isLeaf = False
+            topLeft_g = list(map(lambda a: a[0:len(a)//2],grid[0:gl//2]))  
+            topRight_g = list(map(lambda a: a[len(a)//2:],grid[0:gl//2])) 
+            bottomLeft_g = list(map(lambda a: a[0:len(a)//2],grid[gl//2:]))  
+            bottomRight_g = list(map(lambda a: a[len(a)//2:],grid[gl//2:])) 
+            #print(topLeft)
+            topLeft = self.construct(topLeft_g)
+            topRight = self.construct(topRight_g)
+            bottomLeft = self.construct(bottomLeft_g)
+            bottomRight = self.construct(bottomRight_g)
+            #print(self.construct(topLeft).val)
+        cur = Node(val, isLeaf, topLeft, topRight, bottomLeft, bottomRight)
+        return cur
+        
         
 
