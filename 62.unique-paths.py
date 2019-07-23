9c4573beaa763c786e970b8321677fe085e77d07
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-23 22:08:00 +0800

    udpate 62 Unique Paths

--- 62.unique-paths.py
+++ 62.unique-paths.py
@@ -6,8 +6,25 @@
 
 class Solution(object):
     bookeeper = dict()
-
     #self implemented
+    def uniquePaths(self, m, n):
+        
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        if m == 1 or n == 1:
+            return 1    
+        if (m, n) in self.bookeeper:
+            return self.bookeeper[(m, n)]
+        elif (n, m) in self.bookeeper:
+            return self.bookeeper[(n, m)]
+        else:
+            self.bookeeper[(n, m)] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n -1)
+            self.bookeeper[(m, n)] = self.uniquePaths(m - 1, n) + self.uniquePaths(m, n -1)
+            return self.uniquePaths(m - 1, n) + self.uniquePaths(m, n -1)
+    '''
     def uniquePaths(self, m, n):
         """
         :type m: int
@@ -24,15 +41,7 @@ class Solution(object):
         cur_res = 0
         
         for k in range(1, n + 1):
-            #print((m // 2, k),(m - m // 2, n - k))
             cur_res += self.uniquePaths(m // 2, k) * self.uniquePaths(m - m // 2, n - k + 1)
         return cur_res
-            
-        '''
-            if (m, n - 1) in self.bookeeper:
-                n_bk = self.bookeeper[(m, n - 1)]            
-            else:
-                n_bk = self.uniquePaths(m, n -1)
-            return m_bk * n_bk
-        '''
+    '''        
 
