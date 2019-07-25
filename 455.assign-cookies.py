f1202391bf54f3d1f24484fa895c70f471a1ea93
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-25 23:34:26 +0800

    update 455 Assign Cookies

--- 455.assign-cookies.py
+++ 455.assign-cookies.py
@@ -10,5 +10,18 @@ class Solution(object):
         :type s: List[int]
         :rtype: int
         """
-        
+        g.sort()
+        s.sort()
+        ans = 0
+        cookie = 0
+        while cookie < len(s) and g:
+            if s[cookie] >= g[0]:
+                ans += 1
+                s.pop(0)
+                g.pop(0)
+                cookie -= 1
+            cookie += 1
+        return ans
+
+
 
