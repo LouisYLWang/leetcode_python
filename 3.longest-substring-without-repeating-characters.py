8a4f70be02951d0a8c1f9cd26b6de25dc7c4ad13
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-25 18:05:22 +0800

    update 3 Longest Substring Without Repeating Characters

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -4,24 +4,23 @@
 # [3] Longest Substring Without Repeating Characters
 #
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        max_len = 0
-        cache = 0
-        s_dic = {}
-        j = 0
-        while j < len(s):
-            s[j]
+    def lengthOfLongestSubstring(self, s):
+        left = 0 
+        right = 0
+        char_map = dict([i, 0] for i in s)
+        longest = 0
+        counter = 0
+        while right < len(s):
+            if char_map[s[right]] > 0:
+                if counter > longest:
+                    longest = counter
+                if char_map[s[left]] > 0:
+                    char_map[s[left]] -= 1
+                left += 1
+                counter -= 1
 
-
-                if i in s_dic:
-                    cache = 1
-                    s_dic = {}
-                else:
-                    cache += 1
-                s_dic[i] = 1
-                max_len = max(cache, max_len)
-            j += 1
-        return max_len
-
-
-
+            else:
+                char_map[s[right]] += 1
+                right += 1
+                counter += 1
+        return max(longest, counter)
