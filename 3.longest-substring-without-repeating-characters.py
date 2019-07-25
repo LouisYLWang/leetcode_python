c85a5ff7ad4a95f25e991230ed51f12ebe189365
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-25 20:03:21 +0800

    update 3 Longest Substring Without Repeating Characters

--- 3.longest-substring-without-repeating-characters.py
+++ 3.longest-substring-without-repeating-characters.py
@@ -4,23 +4,54 @@
 # [3] Longest Substring Without Repeating Characters
 #
 class Solution:
+    # self implemented
+    '''
     def lengthOfLongestSubstring(self, s):
         left = 0 
         right = 0
-        char_map = dict([i, 0] for i in s)
+        char_map = set()
         longest = 0
         counter = 0
         while right < len(s):
-            if char_map[s[right]] > 0:
+            if s[right] in char_map:
                 if counter > longest:
                     longest = counter
-                if char_map[s[left]] > 0:
-                    char_map[s[left]] -= 1
+                if s[left] in char_map:
+                    char_map.remove(s[left])
                 left += 1
                 counter -= 1
 
             else:
-                char_map[s[right]] += 1
+                char_map.add(s[right])
                 right += 1
                 counter += 1
         return max(longest, counter)
+    '''
+    # better method O(n)
+    def lengthOfLongestSubstring(self, s):
+        res = 0 
+        hash_ = dict()
+        i = 0
+        for j in range(len(s)):
+            if s[j] in hash_:
+                i = max(hash_[s[j]], i)
+            res = max(res, j - i + 1)
+            hash_[s[j]] = j + 1
+        return res
+    '''
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        window = set()
+        left = 0
+        max_len = 0
+        cur_len = 0
+        for i in range(len(s)):
+            cur_len += 1
+            while s[i] in window:
+                window.remove(s[left])
+                left += 1
+                cur_len -= 1
+            if cur_len > max_len:
+                max_len = cur_len
+            window.add(s[i])
+        return max_len
+    '''
