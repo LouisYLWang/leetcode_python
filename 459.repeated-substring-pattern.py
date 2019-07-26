f7bd32fcce674ed86e30fa4e8886b12a8eb9cfc6
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-26 12:28:06 +0800

    update 459 Repeated Substring Pattern

--- 459.repeated-substring-pattern.py
+++ 459.repeated-substring-pattern.py
@@ -3,11 +3,22 @@
 #
 # [459] Repeated Substring Pattern
 #
-class Solution(object):
-    def repeatedSubstringPattern(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        
 
+class Solution:
+    # slicky method
+    def repeatedSubstringPattern(self, s):
+        return s in (s + s)[1: -1]
+
+    # slow but orthodox
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        len_ = len(s)
+        i = 0
+        for t in range(1, len_//2 + 1):
+            if len_ % t:
+                continue
+            i = t
+            while i < len_ and s[i%t] == s[i]:
+                i += 1
+            if i == len_:
+                return True
+        return False
