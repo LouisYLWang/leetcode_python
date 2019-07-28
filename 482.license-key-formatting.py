b1d4ef4effee486ea29c3c04f93da6d61cdaea67
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-29 00:30:36 +0800

    update 482 License Key Formatting

--- 482.license-key-formatting.py
+++ 482.license-key-formatting.py
@@ -4,14 +4,16 @@
 # [482] License Key Formatting
 #
 class Solution(object):
-  def licenseKeyFormatting(self, S, K):
-        x = 0
-        S = S.replace("-", "").upper()
-        mod = len(S) % K
-        if mod == 0:
-            mod += K
-        while mod < len(S):
-            S = S[:mod] + "-" + S[mod:]
-            mod += K + 1
-        return S
+    def licenseKeyFormatting(self, S, K):
+        ls_S = S.upper().split('-')
+        res = ''.join(ls_S)
+        char_len = len(res)
+        len_1 = char_len % K if char_len % K else K
+        ans = res[0:len_1]
+        i = len_1
+        while i < char_len:
+            ans += '-%s' %res[i:i+K]
+            i += K
+        return ans
+      
 
