31a2809197a2fada816e7ee294c575ff3707e23e
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-29 12:31:05 +0800

    update 485 Max Consecutive Ones

--- 485.max-consecutive-ones.py
+++ 485.max-consecutive-ones.py
@@ -4,6 +4,7 @@
 # [485] Max Consecutive Ones
 #
 class Solution(object):
+    # better method
     def findMaxConsecutiveOnes(self, nums):
         """
         :type nums: List[int]
@@ -19,4 +20,21 @@ class Solution(object):
                     max_len = i - zero_pos - 1
                 zero_pos = i
         return max_len
+    '''
+    def findMaxConsecutiveOnes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_len = 0
+        cur_len = 0 
+        for i in range(len(nums)):
+            if nums[i] == 1:
+                cur_len += 1
+            if nums[i] == 0:
+                if cur_len > max_len:
+                    max_len = cur_len
+                cur_len = 0
+        return max_len if cur_len < max_len else cur_len
+    '''
 
