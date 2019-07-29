8e734e54130eb0e0e1cde6c48d43e9f618cb6a95
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-29 23:49:07 +0800

    update 504 base 7

---
+++ 504.base-7.py
@@ -0,0 +1,13 @@
+#
+# @lc app=leetcode id=504 lang=python
+#
+# [504] Base 7
+#
+class Solution(object):
+    def convertToBase7(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        
+
