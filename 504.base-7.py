ff03fba575075c719ff8e8feb4ba747d8b20583d
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-29 23:57:51 +0800

    update 504 Base 7

--- 504.base-7.py
+++ 504.base-7.py
@@ -2,12 +2,27 @@
 # @lc app=leetcode id=504 lang=python
 #
 # [504] Base 7
-#
+
 class Solution(object):
     def convertToBase7(self, num):
         """
         :type num: int
         :rtype: str
         """
+        res = ''
+        cur = num
+        bits = "0123456"
         
+        if num < 0:
+            cur = -num
+    
+        while cur // 7:
+            res = bits[cur % 7] + res
+            cur //= 7
+            
+        if num < 0:
+            return '-'+ bits[cur] + res
+        else:
+            return bits[cur] + res
+
 
