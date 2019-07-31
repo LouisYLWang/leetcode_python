20d5d9f970123e80891b2bed1a30dbeaf0867df1
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-31 12:06:56 +0800

    665 Non-decreasing Array (fuck the problem)

--- 665.non-decreasing-array.py
+++ 665.non-decreasing-array.py
@@ -1,23 +1,10 @@
 #
-# @lc app=leetcode id=665 lang=python
+# @lc app=leetcode id=665 lang=python3
 #
 # [665] Non-decreasing Array
 #
 class Solution:
-    def checkPossibility(self, nums: List[int]) -> bool:
-        flag = 0
-        i = 0
-        
-        while i < len(nums) -1:
-            if nums[i] > nums[i + 1]:
-                flag += 1
-                if i + 2 < len(nums):
-                    if nums[i+1] < nums[i-1]:
-                        return False
-            i += 1
-        return flag <= 1
-
-    def checkPossibility(self, nums: List[int]) -> bool:
+    def checkPossibility(self, nums):
     	j = 0
     	for i in range(len(nums)-1):
     		if nums[i]-nums[i+1] > 0:
