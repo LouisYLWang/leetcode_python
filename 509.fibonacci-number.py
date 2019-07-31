287d6694480efafb88b64050373d2bbade3e0441
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-31 12:27:06 +0800

    update 509 Fibonacci Number

--- 509.fibonacci-number.py
+++ 509.fibonacci-number.py
@@ -4,6 +4,26 @@
 # [509] Fibonacci Number
 #
 class Solution(object):
+    # memorization 
+    memo = dict()
+    def fib(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        if N == 0:
+            return 0
+        if N == 1:
+            return 1
+        
+        if N in self.memo:
+            return self.memo[N]
+        else:
+            self.memo[N] = self.fib(N - 1) + self.fib(N - 2)
+        return self.memo[N]
+
+    # pure recursion
+    '''
     def fib(self, N):
         """
         :type N: int
@@ -14,4 +34,4 @@ class Solution(object):
         if N == 1:
             return 1
         return self.fib(N - 1) + self.fib(N-2)
-
+    '''
