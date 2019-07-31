4e975905d6adf6ed6819627e222dbd16b20f3b98
louis.yl.wang <louis.yl.wang@outlook.com>
2019-07-31 22:55:06 +0800

    update 322 Coin Change (memorization)

--- 322.coin-change.py
+++ 322.coin-change.py
@@ -8,13 +8,8 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         memo = dict()
         def getMinCoin(coins, amount):
-            print(amount)
             if amount == 0:
                 return 0
-            #if amount < 0 or amount < min(coins):
-            #    return -1
-            if amount in coins:
-                return 1
             if amount in memo:
                 return memo[amount]
             else:
@@ -25,11 +20,8 @@ class Solution:
                     cur = getMinCoin(coins, amount - coin) 
                     if cur == -1: continue
                     min_num = min(min_num, cur + 1)
-
-
                 memo[amount] = min_num
-                return min_num
+                return min_num if min_num != float('inf') else -1
         return getMinCoin(coins, amount)
-        
-        
+
 
