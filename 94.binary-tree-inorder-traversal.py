3c9e5c62072f1569f7de9d4a9f28004a34704ce2
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-04 00:19:50 +0800

    update 94 Binary Tree Inorder Traversal

--- 94.binary-tree-inorder-traversal.py
+++ 94.binary-tree-inorder-traversal.py
@@ -11,6 +11,29 @@
 #         self.right = None
 
 class Solution:
+
+    # best answer "for me"
+    def inorderTraversal(self, root):
+        if not root:
+            return []
+        # 1 表示递归处理
+        stack = [(1, root)]
+        res = []
+        while stack:
+            command, node = stack.pop()
+            if command == 0:
+                # 0 表示当前马上执行将结点的值添加到结果集中
+                res.append(node.val)
+            else:
+                # 关键在这里：因为是模拟系统栈，应该把中序遍历的顺序倒过来写
+                # 调整一下顺序就可以完成前序遍历和后序遍历
+                if node.right:
+                    stack.append((1, node.right))
+                stack.append((0, node))
+                if node.left:
+                    stack.append((1, node.left))
+        return res
+    
     def inorderTraversal(self, root):
         """
         :type root: TreeNode
@@ -29,5 +52,5 @@ class Solution:
             res.append(cur.val)
             cur = cur.right
         return res
-
+    
 
