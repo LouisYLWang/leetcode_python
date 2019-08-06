b6ff9d18d76770ce53c1fcb61cec1078210f4887
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-06 15:31:50 +0800

    update 412 Fizz Buzz

--- 412.fizz-buzz.py
+++ 412.fizz-buzz.py
@@ -1,13 +1,19 @@
 #
-# @lc app=leetcode id=412 lang=python
+# @lc app=leetcode id=412 lang=python3
 #
 # [412] Fizz Buzz
 #
-class Solution(object):
-    def fizzBuzz(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        ans = list()
+        for i in range(1, n + 1):
+            if not i %3 and not i%5:
+                ans.append("FizzBuzz")
+            elif not i%5:
+                ans.append("Buzz")
+            elif not i%3:
+                ans.append("Fizz")
+            else:
+                ans.append(str(i))
+        return ans
 
