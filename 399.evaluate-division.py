b6e14360fc749337f7e6c4e2489430eee4b7b01e
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-10 20:39:40 +0800

    udpate 399 Evaluate Division

--- 399.evaluate-division.py
+++ 399.evaluate-division.py
@@ -33,13 +33,33 @@ class Solution(object):
                     return div_map[i][j]
                 else:
                     for k in div_map[i]:
+                        # use visited to control repeating visit
                         visited.add(i)
                         if k not in visited:
                             temp = get_res(k, j, ans)
+                            # do not mistakenly use if temp 
                             if temp != -1:
                                 return div_map[i][k] * temp
+            # notice: if not find anything, remember to return -1
             return -1.0
-                            
+                                
+        # an alternative way of implementing get_res (more compact)
+        def get_res(i, j, ans):
+            if i not in div_map:
+                return -1.0
+            elif i == j:
+                return 1.0
+            
+            for k in div_map[i]:
+                if j == k:
+                    return div_map[i][j]
+                elif k not in visited:
+                    visited.add(i)
+                    temp = get_res(k, j, ans)
+                    if temp != -1:
+                        return div_map[i][k] * temp
+            return -1.0
+                        
         res =  list()
         for query in queries:
             visited = set()
