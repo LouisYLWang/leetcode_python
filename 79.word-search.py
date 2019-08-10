fcf8b36ae68c9a645a39617ef963bf434922cec4
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-10 23:52:40 +0800

    update 79 Word Search

---
+++ 79.word-search.py
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=79 lang=python
+#
+# [79] Word Search
+#
+class Solution(object):
+    def exist(self, board, word):
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+        """
+        
+
