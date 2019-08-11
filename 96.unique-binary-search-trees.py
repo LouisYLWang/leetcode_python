60a79646f3c318722c1ab6e74935b2e707b58b18
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-11 23:56:26 +0800

    update 96 Unique Binary Search Trees

---
+++ 96.unique-binary-search-trees.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=96 lang=python
+#
+# [96] Unique Binary Search Trees
+#
+class Solution(object):
+    def numTrees(self, n):
+        res = [0] * (n+1)
+        res[0] = 1
+        for i in range(1, n+1):
+            for j in range(i):
+                res[i] += res[j] * res[i-1-j]
+        return res[n]
+        
+
