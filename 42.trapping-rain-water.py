398824589b280a99b9f235279bb247e2882994d5
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-30 23:11:48 +0800

    update 42 Trapping Rain Water

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -3,6 +3,28 @@
 #
 # [42] Trapping Rain Water
 #
+
+# standard two-pointer solution
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        l_max, r_max = 0, 0
+        ans = 0 
+        while l < r:
+            if height[l] < height[r]:
+                if height[l] > l_max:
+                    l_max = height[l]
+                else:
+                    ans += l_max - height[l]
+                l += 1
+            else:
+                if height[r] > r_max:
+                    r_max = height[r]
+                else:
+                    ans += r_max - height[r]
+                r -= 1
+        return ans
+                
 class Solution:
     def trap(self, height):
         def l_r_trap(height):
