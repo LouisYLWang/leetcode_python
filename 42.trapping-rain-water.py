74b7972cc970871945e7aff15ae9f8b35de00cfb
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-30 17:52:23 +0800

    update 42 Trapping Rain Water

--- 42.trapping-rain-water.py
+++ 42.trapping-rain-water.py
@@ -4,17 +4,44 @@
 # [42] Trapping Rain Water
 #
 class Solution:
-    def trap(self, height: List[int]) -> int:
-        loc_max = 0
-        i = 0
-        ans = 0
-        while i < len(height):
-            temp = 0
-            j = i
-            while height[j] < height[i] and j < len(height):
-                temp += height[i] - height[j] 
-                j += 1
-            i = j 
-            ans += temp
-            i += 1
-        return ans
+    def trap(self, height):
+        def l_r_trap(height):
+            i = 0
+            ans = 0
+            while i < len(height) - 1:
+                temp = 0
+                j = i + 1
+                loc_max = height[i]
+                while height[j] < height[i]:
+                    temp += height[i] - height[j] 
+                    if j + 1 < len(height):
+                        j += 1
+                    else:
+                        ans += r_l_trap(height[i:][::-1])
+                        return ans
+
+                ans += temp
+                i = j
+
+            return ans
+        
+        def r_l_trap(height):
+            i = 0
+            ans = 0
+            while i < len(height) - 1:
+                temp = 0
+                j = i + 1
+                loc_max = height[i]
+                while height[j] < height[i]:
+                    temp += height[i] - height[j] 
+                    if j + 1 < len(height):
+                        j += 1
+                    else:
+                        return ans
+
+                ans += temp
+                i = j
+
+            return ans
+        
+        return l_r_trap(height)
