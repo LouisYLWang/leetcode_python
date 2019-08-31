223165e89c284a73932fd2ba89fd532951bf1c61
louis.yl.wang <louis.yl.wang@outlook.com>
2019-08-31 23:25:38 +0800

    update 279 Perfect Squares

---
+++ 279.perfect-squares.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode id=279 lang=python
+#
+# [279] Perfect Squares
+#
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ns_hash = [0 for i in range(n+1)]
+        i = 1
+        while i**2 <= n:
+            ns_hash[i**2] = 1
+            i += 1
+
+        for i in range(1,n+1):
+            if ns_hash[i] != 1:
+                j_min = float('inf')
+                j = 1
+                while j ** 2 <= i:
+                    count = ns_hash[j ** 2] + ns_hash[i - j ** 2]
+                    if count < j_min:
+                        j_min = count
+                    j += 1
+                ns_hash[i] = j_min
+        return ns_hash[n] 
+        
+                
