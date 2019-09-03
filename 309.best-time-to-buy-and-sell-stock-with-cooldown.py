c7c7c06e4d4b12611e887bf1edef4a5213ccce88
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-03 15:30:13 +0800

    update 309 Best Time to Buy and Sell Stock with Cooldown

--- 309.best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309.best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -14,3 +14,13 @@ class Solution(object):
             prev_sell = sell
             sell = max(prev_buy + price, prev_sell)
         return sell
+
+        '''
+        explaination:
+
+        buy[i]  = max(rest[i-1]-price, buy[i-1]) 
+        sell[i] = max(buy[i-1]+price, sell[i-1])
+        rest[i] = max(sell[i-1], buy[i-1], rest[i-1])
+
+        rest[i] = sell[i-1]
+        '''
