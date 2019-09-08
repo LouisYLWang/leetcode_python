3d87877865d8679eb44fc4506bd89a45bdcab6bc
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-09 00:13:24 +0800

    update 443 String Compression

--- 443.string-compression.py
+++ 443.string-compression.py
@@ -1,26 +1,22 @@
 #
-# @lc app=leetcode id=443 lang=python3
+# @lc app=leetcode id=443 lang=python
 #
 # [443] String Compression
 #
-class Solution:
-    def compress(self, chars: List[str]) -> int:
-        char_count = 0
+class Solution(object):
+    def compress(self, chars):
+        char_count = 1
         i = j = 0
         if not chars:
             return 0
         cur_char = chars[0]
+        chars.append(None)
             
-        while i < len(chars):
-            #print(i, chars[i], cur_char, char_count)
-            if i == len(chars)-1:
-                char_count += 1
-                
-            if chars[i] != cur_char or i == len(chars)-1:
-                chars[j] = cur_char 
+        while i < len(chars)-1:
+            if chars[i] != chars[i+1]:
+                chars[j] = chars[i]
                 j += 1             
                 if char_count> 1:
-                    #print(str(char_count))
                     for di in str(char_count):
                         chars[j] = di
                         j += 1
