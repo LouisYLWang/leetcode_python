ba5ff8640a3547b8f76fadac938b7e51ae1bd612
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-21 10:32:00 -0700

    update 416 Partition Equal Subset Sum

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -18,9 +18,10 @@ class Solution(object):
         res_map = [0 for i in range(half + 1)]
         res_map[0] = 1
         for num in nums:
-
             for i in range(half, 0, -1):
                 if i >= num:
+                    # when i == num, set res_map[num] to 1
+                    # reverse iteration to make sure in one iteration, the smaller i result will not change the larger i result
                     res_map[i] = res_map[i] or res_map[i - num]
         return res_map[-1]
         
