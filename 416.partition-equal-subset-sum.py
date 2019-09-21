af70560a7265d472d003c8806dd0afcce675c6a4
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-20 23:59:23 -0700

    udpate 416 Partition Equal Subset Sum

--- 416.partition-equal-subset-sum.py
+++ 416.partition-equal-subset-sum.py
@@ -15,26 +15,15 @@ class Solution(object):
             return False
         
         half = sum(nums) // 2 
-        count = 0
-        
-        res_map = [[0 for i in range(half)]]
-        
-        res_map[0][nums[0]-1] = 1
-        
-        for i in range(1,len(nums)):
-            res_map.append([0 for i in range(half)])
-            #print(nums[:i])
-            for j in nums[:i]:
-                res_map[1][j-1] = 1
-            #print('start', res_map)
-            
-            for k in range(half):   
-                if k >= nums[i-1]:
-                    res_map[1][k] = res_map[0][k] or res_map[0][k - nums[i-1]]
-            #print('end', res_map)
-            res_map.pop(0)
-                 
+        res_map = [0 for i in range(half + 1)]
+        res_map[0] = 1
+        for num in nums:
+
+            for i in range(half, 0, -1):
+                if i >= num:
+                    res_map[i] = res_map[i] or res_map[i - num]
         return res_map[-1]
+        
         
         
         
