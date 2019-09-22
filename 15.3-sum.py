8be86211825d4c9bae3e1f3e5f78fa888b7e9b23
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-21 23:44:15 -0700

    update 15 3Sum

--- 15.3-sum.py
+++ 15.3-sum.py
@@ -11,24 +11,47 @@ class Solution(object):
         """
         
         n = len(nums)
+        
         if n < 3:
             return []
         
         nums.sort()
-        res = set()
+        #res = set()
+        res = list()
         
         for i, a in enumerate(nums[:-2]):
             # i >= 1 to make sure if all element equal to each other
             # a == nums[i-1] to jump over same element
             if i >= 1 and a == nums[i-1]:
                 continue
-            visited = set()
+            
+            l = i + 1
+            r = n - 1
+            
+            while l < r:
+                # using two pointer 
+                if nums[l] + nums[r] + a == 0:
+                    res.append([nums[l], nums[r], a])
+                    l += 1
+                    r -= 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+
+                elif nums[l] + nums[r] + a > 0:
+                    r -= 1
+                else:
+                    l += 1
+                    
+                
+            '''
+            # using hash
+            visited = dict()
             for b in nums[i+1:]:
                 if b not in visited:
                     # if -a-b appear again, get one result
-                    visited.add(-a-b)
+                    visited[-a-b] = 1
                 else:
                     res.add((a, b, -a-b))
+            '''
         return res
-        
 
