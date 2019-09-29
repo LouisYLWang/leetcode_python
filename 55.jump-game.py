5c03e3a89cfa77d639371c9c907e6cf33f4db0df
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-29 00:22:27 -0700

    update 55 Jump Game

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -3,6 +3,23 @@
 #
 # [55] Jump Game
 #
+
+# Java
+
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int cur = nums[n - 1];
+        for (int i = n-1; i >= 0; i--){
+            if (i + nums[i] >= cur){
+                cur = i;
+            }
+        }
+        return cur == 0;
+    }
+}
+
+# Py
 class Solution:
     # top down dp, self solition exceed time limit
     '''
