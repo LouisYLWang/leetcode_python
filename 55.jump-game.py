ffa828f70a0e907af003af962c8ccc07647d52d3
louis.yl.wang <louis.yl.wang@outlook.com>
2019-09-29 10:11:22 -0700

    update 55 jump-game.py

--- 55.jump-game.py
+++ 55.jump-game.py
@@ -7,6 +7,7 @@
 # Java
 
 class Solution {
+    /* method 1 greedy
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int cur = nums[n - 1];
@@ -16,6 +17,27 @@ class Solution {
             }
         }
         return cur == 0;
+    }
+}*/
+    
+    // method 2 dp from top to down
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        nums[n - 1] = -1;
+        for (int i = n - 2; i >= 0; i --){
+            for (int j = 0; j < nums[i] + 1; j ++){
+                //System.out.println("i "+i+" j "+j);
+                if (i+j < n && nums[i] >= 0 && nums[i + j] < 0){
+                    nums[i] *= -1;
+                }
+            }
+        }
+        
+        for (int num: nums){
+            //System.out.print(num);
+        }
+        
+        return nums[0] < 0;
     }
 }
 
