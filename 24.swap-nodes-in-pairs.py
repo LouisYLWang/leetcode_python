59adcbd45853c864f1323b657ed13da5c9c1f29a
louis.yl.wang <louis.yl.wang@outlook.com>
2019-10-22 10:48:44 -0700

    update 24 Swap Nodes in Pairs

---
+++ 24.swap-nodes-in-pairs.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=24 lang=python3
+#
+# [24] Swap Nodes in Pairs
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # recursion - slow
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head:
+            if head.next:
+                newhead = head.next
+
+                temp = head.next.next 
+
+                head.next.next = head
+                head.next = self.swapPairs(temp)
+                return newhead
+            else:
+                return head
+# @lc code=end
+
